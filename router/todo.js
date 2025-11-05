145a99e805454a12d86bf043f60900b5fc963de0
iWasNaive <mrlolohastam@gmail.com>
2025-11-05 14:47:40 +0330

    fix router

--- router/todo.js
+++ router/todo.js
@@ -3,6 +3,6 @@ const todoController = require("./../controllers/todo");
 
 const todoRouter = express.Router();
 
-todoRouter.route("/").get(todoController.GetAll);
+todoRouter.route("/").get(todoController.GetAll).post(todoController.Create);
 
 module.exports = todoRouter;
